cea21e5b40140841478e3bceea53ebdf882a500e
Boniface Mburu <boniface.mburu@student.moringaschool.com>
2025-05-27 23:14:36 +0300

    Article code

---
+++ lib/models/article.py
@@ -0,0 +1,17 @@
+from lib.db.connection import get_connection
+
+class Article:
+    def __init__(self, id=None, title="", author_id=None, magazine_id=None):
+        self.id = id
+        self.title = title
+        self.author_id = author_id
+        self.magazine_id = magazine_id
+
+    def save(self):
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO articles (title, author_id, magazine_id) VALUES (?, ?, ?)",
+                       (self.title, self.author_id, self.magazine_id))
+        conn.commit()
+        self.id = cursor.lastrowid
+        conn.close()
