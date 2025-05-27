a67e3a5c9ad576123486df747ee8c0160053b463
Boniface Mburu <boniface.mburu@student.moringaschool.com>
2025-05-27 23:15:25 +0300

    Debug code

---
+++ lib/debug.py
@@ -0,0 +1,7 @@
+from lib.models.author import Author
+from lib.models.magazine import Magazine
+from lib.models.article import Article
+
+# Use this file to experiment manually
+if __name__ == "__main__":
+    print("Debugging session started...")
